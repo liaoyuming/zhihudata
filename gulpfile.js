2ac7d79f044f91ea0cacc0b5f18a4720a6215e58
sky <mingyuliao@foxmail.com>
2017-05-02 18:42:18 +0800

    Add bower  and  update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 global.isProd = false;
 global.app = require('./package.json');
-global.conf = require('./node_modules/gulpfile-django/gulpvars.json');
+global.conf = require('./gulpvars.json');
 require('./node_modules/gulpfile-django/gulp');
+
+var gulp = require('gulp');
+
+gulp.task('scripts-lib', function() {
+    gulp.src('bower_components/**/*.js')
+        .pipe(gulp.dest(global.conf.path.dist));
+})
